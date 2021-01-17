bf7919b8f5da4b0ec65b78ed9e0bc37e9c2ef175
Sarabjeet Singh <sjsingh200893@gmail.com>
2021-01-17 22:57:57 +0530

    Updated Medium custom url

--- src/components/introduction.js
+++ src/components/introduction.js
@@ -45,7 +45,7 @@ export default class Introduction extends Component {
                                             <div className="slider-text-inner">
                                                 <div className="desc">
                                                     <h1>I often <br />Write ... </h1>
-                                                    <p><a className="btn btn-primary btn-learn" href="https://medium.com/@sjsingh200893" target="_blank" rel="noopener noreferrer">View Blog <i className="icon-book" /></a></p>
+                                                    <p><a className="btn btn-primary btn-learn" href="https://sarabjeet-singh.medium.com/" target="_blank" rel="noopener noreferrer">View Blog <i className="icon-book" /></a></p>
                                                 </div>
                                             </div>
                                         </div>
