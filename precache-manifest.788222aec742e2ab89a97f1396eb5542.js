8c5005cc0ba131126bd95fe494bb6937a51a9537
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-01-17 17:29:36 +0000

    Deploy singh-sarabjeet/me to github.com/singh-sarabjeet/me.git:gh-pages

---
+++ precache-manifest.788222aec742e2ab89a97f1396eb5542.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a149b660f14f9051f1e219dac55c159d",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "e476d636266a215cd4a7",
+    "url": "/me/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fb0e86f4193336d1f4ce",
+    "url": "/me/static/js/2.55c92afe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/me/static/js/2.55c92afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e476d636266a215cd4a7",
+    "url": "/me/static/js/main.7c1e74c1.chunk.js"
+  },
+  {
+    "revision": "45f033d185bd3fc94ae0",
+    "url": "/me/static/js/runtime-main.50e3daf3.js"
+  }
+]);
